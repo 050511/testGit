4a71015716c2040bc8c132d6eb10be768278bd39
sf <2920530493@qq.com>
2023-06-28 09:52:21 +0800

    ·

--- src/main/java/com/suifeng/main.java
+++ src/main/java/com/suifeng/main.java
@@ -2,6 +2,6 @@ package com.suifeng;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("HelloGit");
+        System.out.println("Helloworld");
     }
 }
